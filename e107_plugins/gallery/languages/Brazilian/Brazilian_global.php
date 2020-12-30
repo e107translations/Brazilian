f5286124cbce75887a41ba0c497e503e7d09cd6d
Barbara Samel Rocha Tostes <barbaratostes@gmail.com>
2020-12-30 17:14:16 -0300

    Gallery Brazilian FIXED
    Report ok now

--- e107_plugins/gallery/languages/Brazilian/Brazilian_global.php
+++ e107_plugins/gallery/languages/Brazilian/Brazilian_global.php
@@ -5,19 +5,15 @@
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
 |
-|        $URL: ../e107_plugins/gallery/languages/Portuguese/Portuguese_global.php $
+|        $URL: ../e107_plugins/gallery/languages/Brazilian/Brazilian_global.php $
 |        $Revision: 1.0 $
 |        $Id: 2015/02/15 21:00:57 $
 |        $Author: Barbara $
 +---------------------------------------------------------------+
 */
-
 define("LAN_PLUGIN_GALLERY_TITLE", "Galeria");
 define("LAN_PLUGIN_GALLERY_DIZ", "Uma galeria de imagens simples");
-
-
-?>define("LAN_PLUGIN_GALLERY_SEF_01", "Galeria SEF");
-define("LAN_PLUGIN_GALLERY_SEF_02", "SEF URLs habilitado.");
-define("LAN_PLUGIN_GALLERY_SEF_03", "SEF URLs desativados.");
-define("LAN_PLUGIN_GALLERY_SEF_04", "Padrão de galeria");
-define("LAN_PLUGIN_GALLERY_SEF_01", "Galeria SEF");
+define("LAN_PLUGIN_GALLERY_SEF_01", "SEF da Galeria");
+define("LAN_PLUGIN_GALLERY_SEF_02", "URLs SEF habilitados");
+define("LAN_PLUGIN_GALLERY_SEF_03", "URLs SEF desativados");
+define("LAN_PLUGIN_GALLERY_SEF_04", "Galeria Padrão");
